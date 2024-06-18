f373d2b81592b19d7a8fd5213022272848866711
Aliyah Sausan Huwel <33594713+sausans@users.noreply.github.com>
2024-06-18 17:43:44 -0400

    Update app.py
    adjust the system and user message for the upload image part

--- amaya_env/app.py
+++ amaya_env/app.py
@@ -86,9 +86,8 @@ def chain_of_thought_prompting(chat_text, similar_docs, user_question):
     try:
         recommendation_context = "\n".join(similar_docs)
         messages = [
-            {"role": "system", "content": "You are an expert in love advice. You know how to help people to communicate better to their special person. The steps to do them: 1. Think about the mood and context of the conversation, 2. Identify the communication style used in the conversation text, 3. Based on the mood and context, create potential chat responses, and 4. Adjust the potential chat responses based on the identified communication style. Only share the list of potential responses, no need to give the reasons. "},
-            {"role": "user", "content": f"""Read the following chat conversation. If user has specific question, answer them based on Chat Conversation and User's Specific Question. If user doesn't have a specific question, provide potential responses to the chat conversation:
-                
+            {"role": "system", "content": "You are an expert in love advice. You know how to help people to go through their love journey - either by giving them advices based on chat conversation they shared or give advices on how to communicate better to their special person. The steps to do them: 1. Think about the mood and context of the conversation, 2. Identify the communication style used in the conversation text, 3. If the user asks about how to respond to the chat,  create potential chat responses, and adjust the potential chat responses based on the identified communication style and recommendation. Only share the list of potential responses, no need to give the reasons., 4. If not, based on the mood, context, and the user’s question, answer the them accordingly."},
+            {"role": "user", "content": f"""Read the following chat conversation, user's specific question, and recommendation:                
             Chat Conversation:
             {chat_text}
 
