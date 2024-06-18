01cf8bd89dd67da089bacf345d0954f61bc55240
Aliyah Sausan Huwel <33594713+sausans@users.noreply.github.com>
2024-06-18 18:30:38 -0400

    Update app.py
    better flow for image processing

--- amaya_env/app.py
+++ amaya_env/app.py
@@ -163,9 +163,7 @@ def chat():
             st.session_state["uploaded_image"] = uploaded_image
             image = Image.open(uploaded_image)
             extracted_text = extract_text_from_image(image)
-            extracted_text_for_api = "Here is extracted message from the screenshot. This is just for your reference if user asked, no need to respond right away:\n" + extracted_text
             st.session_state["extracted_text"] = extracted_text
-            st.session_state.messages.insert(0, {"role": "system", "content": extracted_text_for_api})
             st.session_state["image_processed"] = True  # Flag that image has been processed
             st.session_state.messages.append({"role": "assistant", "content": "I've read your text. What would you like to ask?"})
     
@@ -204,6 +202,8 @@ Tell me what’s going on! If you upload a screenshot of your chat with that spe
         if st.session_state.get("image_processed", False):
             # Answer the user after processing the image
             drafts = generate_drafts(st.session_state["extracted_text"], prompt)
+            extracted_text_for_api = "Here is extracted message from the screenshot. This is just for your reference if user asked, no need to respond right away:\n" + st.session_state["extracted_text"]
+            st.session_state.messages.insert(0, {"role": "system", "content": extracted_text_for_api})
             response_text = "\n".join(drafts)
             #response_text = "Here are some suggestions:\n" + "\n".join(drafts)
             #st.session_state.messages.append({"role": "assistant", "content": "Here are some suggestions:"})
@@ -211,8 +211,8 @@ Tell me what’s going on! If you upload a screenshot of your chat with that spe
             st.markdown(response_text)
             st.session_state.messages.append({"role": "assistant", "content": response_text})
             st.session_state["image_processed"] = False  # Reset the flag
-            st.session_state["uploaded_image"] = None
-            uploaded_image = None 
+            #st.session_state["uploaded_image"] = None
+            
         else: 
             with st.chat_message("assistant", avatar=avatar_url):
                 response = openai.ChatCompletion.create(model=st.session_state["openai_model"],
